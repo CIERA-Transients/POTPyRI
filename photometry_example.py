1a773b79f59f6cea15a0faa96939e33ccff4e1c3
Charlie Kilpatrick <cdkilpat@ucsc.edu>
2021-05-20 17:24:56 -0500

    Updating Binospec left side mask file

--- photometry_example.py
+++ photometry_example.py
@@ -3,8 +3,8 @@ from absphot import *
 
 def main():
 
-    do_phot('2020jfo.g.ut200621.051434_1.sw.fits')
+    #do_phot('2020jfo.g.ut200621.051434_1.sw.fits')
     ap = absphot()
-    ap.find_zeropoint('2020jfo.g.ut200621.051434_1.sw.pcmp', 'g', 'PS1')
+    ap.find_zeropoint('sGRB140930B_J_J_wcs.pcmp', 'J', '2MASS')
 
 main()
