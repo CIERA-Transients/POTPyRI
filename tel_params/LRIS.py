1d0f539261aa8c090c565ab1c880fe20d865d068
Kerry Paterson <kerry.paterson@northwestern.edu>
2021-08-18 14:23:47 -0500

    Fixed small typo.

--- tel_params/LRIS.py
+++ tel_params/LRIS.py
@@ -216,7 +216,7 @@ def process_science(sci_list,fil,amp,binn,red_path,mbias=None,mflat=None,proc=No
                 trim_sec = [mbias[k].header['DATASEC'] for k in range(len(mbias))]
             else:
                 trim_sec = [hdr[k+1].header['DATASEC'] for k in range(len(mbias))]
-                for k,x in enumerate(mbias)):
+                for k,x in enumerate(mbias):
                     x.data = x.data[0:int(trim_sec[k].split(':')[-1].rstrip(']')),0:int(trim_sec[k].split(':')[1].split(',')[0])]
                     mbias[k] = x
         red = [ccdproc.ccd_process(x, oscan=oscan_reg, oscan_model=models.Chebyshev1D(3), trim=trim_sec[k], gain=gains[k]*u.electron/u.adu, readnoise=readnoises[k]*u.electron, master_bias=mbias[k], gain_corrected=True) for k,x in enumerate(raw)]
