4e8b6bb7aa7f1d26cbabd30ae097c3b627ff460e
charliekilpatrick <ckilpatrick@gmail.com>
2024-11-23 11:10:45 -0500

    Adding F2 to instruments

---
+++ potpyri/instruments/F2.py
@@ -0,0 +1,125 @@
+# Parameter file for F2/Gemini-S
+
+__version__ = "1.0" # Last edited 11/23/2024
+
+import os
+import ccdproc
+import numpy as np
+
+import astropy.units as u
+from astropy.io import fits
+from astropy.time import Time
+from astropy.nddata import CCDData
+
+# Internal dependency
+from . import instrument
+
+class F2(instrument.Instrument):
+
+    def __init__(self):
+
+        self.version = __version__
+
+        self.name = 'F2'
+
+        # Extensions for keeping track of general metadata and WCS
+        self.raw_header_ext = 0
+        self.wcs_extension = 0
+
+        # Detector specific characteristics
+        self.pixscale = 0.1792
+        self.saturation = 65000.0
+
+        self.min_exptime = 1.0
+
+        # Run dark/bias/flat calibration?
+        self.dark = True
+        self.bias = False
+        self.flat = True
+
+        # Parameters for handling calibration files
+        # Run rejection on possible CR pixels in bias
+        self.cr_bias = True 
+
+        # How to combine images during stacking
+        self.stack_method = 'median'
+
+        self.wavelength = 'NIR'
+
+        self.gain = None
+        self.rdnoise = None
+
+        self.datasec = None
+        self.biassec = None
+
+        # Keywords for selecting files from Sort_files object
+        # This allows a single file type to be used for multiple purposes (e.g., for
+        # generating a flat-field image from science exposures)
+        self.filetype_keywords = {'SCIENCE':'SCIENCE', 'FLAT':'[SCIENCE,FLAT]', 
+            'DARK':'DARK','BIAS':'BIAS'}
+
+        # Header keywords
+        self.target_keyword = 'OBJECT'
+        self.exptime_keyword = 'EXPTIME'
+        self.filter_keyword = 'FILTER'
+        self.mjd_keyword = 'MJD-OBS'
+        self.bin_keyword = '11'
+        self.amp_keyword = '1'
+
+        # File sorting keywords
+        self.science_keywords = ['GCALSHUT','MASKNAME']
+        self.science_values = ['OPEN','None']
+        self.flat_keywords = ['OBJECT']
+        self.flat_values = ['GCALflat']
+        self.bias_keywords = []
+        self.bias_values = []
+        self.dark_keywords = ['FILTER','OBSTYPE']
+        self.dark_values = ['Dark','DARK']
+        self.spec_keywords = ['GRORDER']
+        self.spec_values = ['1']
+        self.bad_keywords = ['MASKNAME']
+        self.bad_values = ['closed']
+
+        self.detrend = True
+        self.catalog_zp = '2MASS'
+
+        self.out_size = 2500
+
+    def get_saturation(self, hdr):
+        return(self.saturate*hdr['NREADS'])
+
+    def raw_format(self, proc):
+        return('*.fits.bz2')
+
+    def get_filter(self, hdr):
+        filt = hdr['FILTER'].replace(' ','').split('_')[0]
+        if filt == 'Ks':
+            filt = 'K'
+        return(filt)
+
+    def get_rdnoise(self, hdr):
+        return(float(hdr['RDNOISE']))
+
+    def get_gain(self, hdr):
+        return(float(hdr['GAIN']))
+
+    def get_exptime(self, hdr):
+        return(hdr['EXPTIME'])
+
+    # Get a unique image number that can be derived only from the file header
+    def get_number(self, hdr):
+        datestr = hdr['DATE-OBS']+'T'+hdr['TIME-OBS']
+        elap = Time(datestr)-Time('1980-01-01')
+        elap = int(np.round(elap.to(u.second).value))
+        return(elap)
+
+    def import_image(self, filename, amp, log=None):
+
+        raw = CCDData.read(filename, unit=u.adu)
+        red = ccdproc.ccd_process(raw, 
+            gain=self.get_gain(raw.header)*u.electron/u.adu, 
+            readnoise=self.get_rdnoise(raw.header)*u.electron)
+
+        red.header['SATURATE'] = self.get_saturation(red.header)
+
+        return(red)
