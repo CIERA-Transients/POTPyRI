1997bffc8104d6464c65a7f3479ae3a13d916354
charliekilpatrick <ckilpatrick@gmail.com>
2025-06-08 10:53:44 -0500

    Updating instruments initialization and options

---
+++ potpyri/instruments/__init__.py
@@ -0,0 +1,12 @@
+from . import BINOSPEC
+from . import DEIMOS
+from . import F2
+from . import FOURSTAR
+from . import GMOS
+from . import IMACS
+from . import LRIS
+from . import MMIRS
+from . import MOSFIRE
+
+# New instruments need to be added here
+__all__ = ["BINOSPEC","DEIMOS","FOURSTAR","GMOS","IMACS","LRIS","MMIRS","MOSFIRE"]
