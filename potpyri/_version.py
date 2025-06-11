e34f5551f71a80f6bc3574429ca5a69ecd03aac4
charliekilpatrick <ckilpatrick@gmail.com>
2025-06-11 16:22:22 -0500

    Updating pyproject.toml

--- potpyri/_version.py
+++ potpyri/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '1.0.0'
-__version_tuple__ = version_tuple = (1, 0, 0)
+__version__ = version = '1.0.2.dev4'
+__version_tuple__ = version_tuple = (1, 0, 2, 'dev4')
