3dbe41c2f28ad92e80b01475449ba0bc806bff0e
charliekilpatrick <ckilpatrick@gmail.com>
2025-07-05 12:44:15 -0500

    Fixing LRIS import for incorrect image size

--- potpyri/instruments/LRIS.py
+++ potpyri/instruments/LRIS.py
@@ -185,6 +185,8 @@ class LRIS(instrument.Instrument):
         amp = self.get_ampl(hdr)
         binn = int(self.get_binning(hdr)[0])
 
+        imsize = (hdr['NAXIS1'], hdr['NAXIS2'])
+
         if amp=='4B_SINGLE_A':
             oscan_reg = '[1:50,1:4096]'
         if amp=='4R_HSPLIT_VSPLIT':
@@ -201,6 +203,10 @@ class LRIS(instrument.Instrument):
             b3=np.max([int(1./binn),1])
             b4=int(4248./binn)
             oscan_reg = f'[{b1}:{b2},{b3}:{b4}]'
+
+            # Correct for image size
+            if b4 < imsize[1]:
+                b4 = imsize[1]
         
         return(oscan_reg)
 
