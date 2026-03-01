6a955ee8d700ee05c0372577d20126a5c9a7c21d
charliekilpatrick <ckilpatrick@gmail.com>
2026-03-01 15:53:36 -0600

    Refactor file processing functions to streamline logic and improve performance. Enhanced modularity and readability of the codebase, ensuring better maintainability for future updates.

--- potpyri/_version.py
+++ potpyri/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.0.14.dev19'
-__version_tuple__ = version_tuple = (1, 0, 14, 'dev19')
+__version__ = version = '1.0.14.dev66'
+__version_tuple__ = version_tuple = (1, 0, 14, 'dev66')
 
-__commit_id__ = commit_id = 'gcb4d08580'
+__commit_id__ = commit_id = 'ge0a1856e2'
