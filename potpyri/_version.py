2a1b3da4a104376534192d92dfaa509c73d24d00
charliekilpatrick <ckilpatrick@gmail.com>
2026-06-17 17:18:26 -0500

    Refactor optical background subtraction integration and enhance command-line options
    - Updated the `process_science` method to fully integrate the `apply_optical_background_subtraction` functionality, replacing the deprecated `skip_skysub` parameter.
    - Enhanced command-line options in `init_options` to allow users to specify background subtraction modes via `--bkg-sub`.
    - Modified the `image_proc` function to accept and pass the new `bkg_sub` parameter for improved flexibility.
    - Added unit tests to ensure the correct behavior of the updated background subtraction features and their integration with existing functionality.

--- potpyri/_version.py
+++ potpyri/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '1.0.17.dev18'
-__version_tuple__ = version_tuple = (1, 0, 17, 'dev18')
+__version__ = version = '1.0.17.dev27'
+__version_tuple__ = version_tuple = (1, 0, 17, 'dev27')
 
-__commit_id__ = commit_id = 'g185939e0b'
+__commit_id__ = commit_id = 'g8eb287bda'
